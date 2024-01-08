880015316853526a3a9283b3b30955dc136f5f24
gost1212 <saadalgamdi888@gmail.com>
2024-01-08 22:18:18 +0300

    add: added 'secret number' while checking user guesses.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
 
-    let secretNum = rand::thread_rng()
+    let secret_num: u32 = rand::thread_rng()
     .gen_range(1..=100);
+
+    print!("Secret number is: {secret_num}\n");
 
     println!("Enter a number: ");
 
@@ -14,7 +17,18 @@ fn main() {
     io::stdin()//use IO
     .read_line(&mut guess)//read line
     .expect("Failed to read line.");//if error, print this
-    //interesting way of codingcr
+    //interesting way of coding
+
+    let guess: u32 = guess
+    .trim()
+    .parse()
+    .expect("Please type a number.");
 
     println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_num){
+        Ordering::Greater => print!("Too Big."),
+        Ordering::Equal => print!("You win!"),
+        Ordering::Less => print!("Too Small.")
+    }
 }
