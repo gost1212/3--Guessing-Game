943f3069cd5b80325d83a69e4c3e8541582eb003
gost1212 <saadalgamdi888@gmail.com>
2024-01-08 21:42:38 +0300

    add: added 'rand' package

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+
+    let secretNum = rand::thread_rng()
+    .gen_range(1..=100);
 
     println!("Enter a number: ");
 
@@ -10,7 +14,7 @@ fn main() {
     io::stdin()//use IO
     .read_line(&mut guess)//read line
     .expect("Failed to read line.");//if error, print this
-    //interesting way of coding
+    //interesting way of codingcr
 
     println!("You guessed: {guess}");
 }
