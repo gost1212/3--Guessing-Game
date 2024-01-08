aa4e2300de192ac30b9a5fc09d5f2c798f80fa63
gost1212 <saadalgamdi888@gmail.com>
2024-01-08 22:42:42 +0300

    add: added comments

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::cmp::Ordering;
 fn main() {
     println!("Guess the number!");
 
-    let secret_num: u32 = rand::thread_rng()
+    let secret_num: u32 = rand::thread_rng()//defning an unsigned 32bit value, with a generated 1..100 number.
     .gen_range(1..=100);
 
     println!("Secret number is: {secret_num}");
@@ -18,14 +18,10 @@ fn main() {
 
         io::stdin()//use IO
         .read_line(&mut guess)//read line
-        .expect("Failed to read line.");//if error, print this
+        .expect("Failed to read line.");//if input read failed, print this
         //interesting way of coding
 
-        /*let guess: u32 = guess
-        .trim()
-        .parse()
-        .expect("Please type a number.");*/
-
+        //convert string input to number
         let guess: u32 = match guess
         .trim()
         .parse(){
@@ -36,13 +32,14 @@ fn main() {
             }
         };
 
+        //compare input number with secret number
         match guess.cmp(&secret_num){
-            Ordering::Greater => println!("Too Big.\n"),
-            Ordering::Equal => {
+            Ordering::Greater => println!("Too Big.\n"),//if input is bigger
+            Ordering::Less => println!("Too Small.\n"),//if input is smaller
+            Ordering::Equal => {//if input is equal
                 println!("You win!\n");
                 break;
-            },
-            Ordering::Less => println!("Too Small.\n")
+            }
         }
         
     }
