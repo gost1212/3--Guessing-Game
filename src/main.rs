da8d71e7ab6cbf064f7c06fa2fb20334516fa7d8
gost1212 <saadalgamdi888@gmail.com>
2024-01-08 19:28:56 +0300

    add: added basic input/outpu string code.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Enter a number: ");
+
+    let mut guess = String::new();//create String variable
+
+    io::stdin()//use IO
+    .read_line(&mut guess)//read line
+    .expect("Failed to read line.");//if error, print this
+    //interesting way of coding
+
+    println!("You guessed: {guess}");
 }
---
+++ target/debug/deps/guessing_game-aca528a814aa643f.d
@@ -0,0 +1,5 @@
+C:\Users\Saad_\Documents\Rust\Excersises\3--Guessing-Game\target\debug\deps\libguessing_game-aca528a814aa643f.rmeta: src\main.rs
+
+C:\Users\Saad_\Documents\Rust\Excersises\3--Guessing-Game\target\debug\deps\guessing_game-aca528a814aa643f.d: src\main.rs
+
+src\main.rs:
